bdf44512e80c25f458071c660f803b2dbfb5ad39
aadyasingh2 <aadya2630@gmail.com>
2026-02-12 11:44:40 +0530

    done

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-let addButton=document.getElementById('adding')
+let bookmarks=  JSON.parse(localStorage.getItem('bookmark')) || []
+let addButton=document.getElementById('my-button')
 let url=document.getElementById('url')
 let nameButton=document.getElementById('name')
 let display=document.getElementById('display')
-addButton.addEventListener('click',function(){
-    let name=nameButton.value
-    let urlvalue=url.value
-    if (name === '' || urlvalue === '') {
-        alert('Please fill in both fields!')
-    return
-}
+function BookmarkAddition(naming,urlvalue){
     let card=document.createElement('div')
-    card.innerHTML=card.innerHTML = `
-    <img src="https://www.google.com/s2/favicons?domain=${urlvalue}&sz=64">
-    <p>${name}</p>
-    <button class="visit">Visit</button>
-    <button class="delete">Delete</button>`
+    card.innerHTML=`<img src="https://www.google.com/s2/favicons?domain=${urlvalue}&sz=64">
+                    <p>${naming}</p>
+                    <button class="visit">Visit</button>
+                    <button class="delete">Delete</button>`
     display.appendChild(card)
+    bookmarks.push({'name':naming,'url':urlvalue})
+    localStorage.setItem('bookmark',JSON.stringify(bookmarks))
     card.querySelector('.visit').addEventListener('click',function(){
         window.open(urlvalue,'_blank')
     })
     card.querySelector('.delete').addEventListener('click',function(){
         card.remove()
     })
-})
-
+}
+addButton.addEventListener('click',function(){
+    let name=nameButton.value
+    let urlvalue=url.value
+    if (name === '' || urlvalue === '') {
+        alert('Please fill in both fields!')
+        return
+    }
+    BookmarkAddition(name,urlvalue)
+    }
+)
+bookmarks.forEach(element => {
+    BookmarkAddition(element.name,element.url)
+});
